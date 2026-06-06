534ba1fe14c8045a280dab56ac2cc263da72025e
Tiago Pujia <tiagonahuelpujia@gmail.com>
2026-06-05 21:16:00 -0300

    Creacion del script

---
+++ main.js
@@ -0,0 +1,13 @@
+const USUARIOS_MAXIMO = 15; // Cantidad de usuarios maximos para irse
+const INTERVALO_MS = 1000; // En milisegundos
+
+const intervalo = setInterval(() => {
+    const tagBotonExit = document.querySelector("#hangup-button");
+    const tagCantidadUsuarios = document.querySelector("[data-tid='roster-button-tile']");
+    const cantidadUsuarios = !tagCantidadUsuarios ? 1 : parseInt(tagCantidadUsuarios.textContent, 10);
+
+    if (cantidadUsuarios < USUARIOS_MAXIMO) {
+        tagBotonExit.click();
+        clearInterval(intervalo);
+    }
+}, INTERVALO_MS);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Como usarlo
+1. Entrar a la llamada por navegador
+2. Esperar a que se conecten la mayoria de los participantes
+3. Abrir la consola del navegador (F12 o Ctrl + Shift + I)
+4. Copiar y pegar el siguiente código de `main.js` en la consola y presionar Enter
